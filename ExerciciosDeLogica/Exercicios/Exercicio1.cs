24f7575a003e9006d99ca6e162ed0580b322a41e
Eudis <eudes.eds@gmail.com>
2022-08-10 00:55:05 -0300

    Commit Inicial - Execicio de Logica1

---
+++ ExerciciosDeLogica/Exercicios/Exercicio1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExerciciosDeLogica;
+
+namespace ExerciciosDeLogica.Exercicios
+{
+    public class Exercicio1
+    {
+        public Exercicio1()
+        {
+            Conversoes();
+        }
+
+        public void Conversoes()
+        {
+            Console.WriteLine("Selecione a Conversão desejada:");
+            Console.WriteLine("1 - Conversão de Celsius para Fahrenheit");
+            Console.WriteLine("2 - Conversão de Fahrenheit para Celsius");
+            Console.WriteLine("0 - Voltar ao Menu");
+            int n;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 0:
+                        Console.Clear();
+                        Program.Menu();
+                        break;
+                    case (int)1:
+                        ConverterCelsiusParaFahrenheit();
+                        break;
+                    case (int)2:
+                        ConverterFahrenheitParaCelsius();
+                        break;
+                }
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Informe apenas números de 0 a 2");
+                Conversoes();
+            }
+        }
+
+        public void ConverterCelsiusParaFahrenheit()
+        {
+
+        }
+        public void ConverterFahrenheitParaCelsius()
+        {
+
+        }
+    }
+}
---
+++ ExerciciosDeLogica/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ExerciciosDeLogica.Exercicios;
+
+namespace ExerciciosDeLogica
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("Selecione o Exercicio:");
+            Console.WriteLine("1 - Conversão entre Celsius e Fahrenheit");
+            Console.WriteLine("2 - ");
+            Console.WriteLine("3 - ");
+            Console.WriteLine("4 - ");
+            Console.WriteLine("5 - ");
+            int n;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n) { 
+                    case (int)1:
+                        Console.WriteLine("");
+                        var exerc = new Exercicio1();
+                        break;
+                }
+                Menu();
+            }
+            catch
+            {
+                Console.WriteLine("Informe apenas números de 1 a 5");
+                Menu();
+            }
+        }
+    }
+}
